fba852aaf2a443e0d16905086b2de51cf6ae087e
Ramon150908 <163332543+Ramon150908@users.noreply.github.com>
2025-02-13 18:22:46 -0300

    Add files via upload
    ................................

---
+++ aula02/exemplo01.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 02 - SW I </title>
+</head>
+<body>
+    <p>Oi Anderson</p>
+
+    <?php
+        $nome = "Ramon";
+
+        echo "<p>Oi Anderson, eu sou o Ramon</p>";
+        echo "<p>Oi Anderson, eu sou o $nome</p>";
+        echo "<p>Oi Anderson, eu sou o ".$nome."</p>";
+    ?>
+
+</body>
+</html>
