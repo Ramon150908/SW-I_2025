e93d882287cef5b107519bd6cc2ff7ab9133ca36
Ramon150908 <163332543+Ramon150908@users.noreply.github.com>
2025-02-28 17:00:56 -0300

    Add files via upload

---
+++ aula03/processa01.php
@@ -0,0 +1,8 @@
+<?php 
+    $nome = $_GET['cxnome'];
+    echo "Bem Vindo, $nome";
+
+
+    
+
+?>
---
+++ aula03/processa_EX.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina processa</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+
+
+
+
+<?php
+    $nome = $_POST["cxnome"];
+    $msg =  $_POST["cxmsg"];
+    echo "Bem Vindo, ", $nome,", ",$msg;
+
+
+
+?>
