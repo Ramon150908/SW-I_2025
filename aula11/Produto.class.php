0491ce25ef42cf0a5d645b364d797110d151754b
Ramon150908 <163332543+Ramon150908@users.noreply.github.com>
2025-05-24 20:50:12 -0300

    ..............

--- aula11/Produto.class.php
+++ aula11/Produto.class.php
@@ -12,14 +12,36 @@
         public function getNome(){
             return $this->Nome;
         }
-        public function setNome(){
+        public function setNome($nome){
             $this->Nome = $nome;
         }
         public function getPreco(){
             return $this->Preco;
         }
-        public function getQtde(){
-            return $this->Qtde;
+        public function setPreco($preco){
+            $this->Preco = $preco; 
+        }
+
+        public function addestoque($qtde){
+            if ($qtde < 0) {
+                echo"ERRO!";
+            }else{
+                $this->Qtde += $qtde;
+            }
+        }
+        
+        public function Removerestoque($qtde){
+            if ($qtde < 0) {
+                echo"ERRO!";
+            }else{
+                $this->Qtde -= $qtde;
+            }  
+        } 
+        
+        public function mostrarDetalhes(){
+            echo"Produto: " . $this->Nome . "<br><hr>";
+            echo"Preço: " . $this->Preco . "<br><hr>";
+            echo"Quantidade em estoque: " . $this->Qtde . "<br><hr>";
         }
 
     }
--- aula11/exercicio.php
+++ aula11/exercicio.php
@@ -1,9 +1,9 @@
 <?php
+    include_once "Produto.class.php";
+    $produto = new Produto("Mouse",124.99,15);
 
-
-
-
-
-
-
+    //$produto->setPreco(124.99);
+    $produto->addestoque(13);
+    $produto->removerestoque(3);
+    $produto->mostrarDetalhes();
 ?>
